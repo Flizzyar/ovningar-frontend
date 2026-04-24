938bf778fe08446e32a11ebd826e8970b460ef2f
Flizzyar <felisiatrense@gmail.com>
2026-04-24 08:40:49 +0200

    Ovning12

---
+++ Ovning12/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div style={{ padding: '2rem', textAlign: 'center' }}>
+            <h1>Min första PWA 🚀</h1>
+            <p>Testa att installera mig!</p>
+        </div>
+    )
+}
